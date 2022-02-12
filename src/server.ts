490410328901dee278780530adb31293f0fd4641
ismetkoralay <ismetkoralay@gmail.com>
2022-02-12 17:55:00 +0300

    mongodb connection set up

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
 import { app } from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const port = 4000;
 
 const startApp = async () => {
+
+    const connectionString = process.env.MONGO_URL;
+    if (!connectionString) {
+        throw new Error("You must provide MONGO_URL from environment");
+    }
+
+    try {
+        await mongoose.connect(connectionString);
+        console.log("Mongo connected");
+    } catch (error) {
+        console.log(error);
+    }
 
     app.listen(port, () => {
         console.log("Listening on port 4000");
